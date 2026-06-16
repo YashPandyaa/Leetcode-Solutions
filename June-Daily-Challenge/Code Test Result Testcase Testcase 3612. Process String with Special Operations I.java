6fc795528818d64511c6849e0d804ce5bc04e242
Yash Pandya <yashpandya2223@gmail.com>
2026-06-16 15:20:04 +0530

    Create  Code Test Result Testcase Testcase 3612. Process String with Special Operations I.java

---
+++ June-Daily-Challenge/Code Test Result Testcase Testcase 3612. Process String with Special Operations I.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '#') {
+                ans.append(ans.toString());
+            }
+            else if (ch == '*') {
+                if (ans.length() > 0) {
+                    ans.deleteCharAt(ans.length() - 1);
+                }
+            }
+            else if (ch == '%') {
+                ans.reverse();
+            }
+            else {
+                ans.append(ch);
+            }
+        }
+
+        return ans.toString();
+    }
+}
